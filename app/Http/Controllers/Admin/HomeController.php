fbe57ba3a360ebdfaad69ad89050a55afce9d13e
Muhammad Toqeer Iqbal <31422464+mtoqeer@users.noreply.github.com>
2019-02-15 23:14:40 +0500

    Admin Front Page Added.

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index() {
-        return view('admin.home');
+        return view('admin.dashboard.home');
     }
 
 }
