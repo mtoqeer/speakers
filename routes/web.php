f01f9b02ce75847c76b1444d93ef4c0c2d40bd8c
Muhammad Toqeer Iqbal <31422464+mtoqeer@users.noreply.github.com>
2019-03-13 18:37:47 +0500

    Middleware change to Verified for User Dashboard

--- routes/web.php
+++ routes/web.php
@@ -45,92 +45,87 @@ Route::get('/dashboard', 'HomeController@index')->name('userdashboard')->middlew
 
 
 // User User Meta Routes
-Route::get('dashboard/generalinfo', 'UserMetaController@userMetaIndex')->name('dashboard.generalinfo')->middleware('auth');
-Route::post('dashboard/generalinfo', 'UserMetaController@userMetaSave')->name('userMetaSave')->middleware('auth');
+Route::get('dashboard/generalinfo', 'UserMetaController@userMetaIndex')->name('dashboard.generalinfo')->middleware('verified');
+Route::post('dashboard/generalinfo', 'UserMetaController@userMetaSave')->name('userMetaSave')->middleware('verified');
 // Route::get('dashboard/generalinfo/{id}', 'UserMetaController@userMetaDelete')->name('userMetaDelete')->middleware('auth');
 // User Meta Update Reqest
-Route::post('/dashboard/generalinfo/update', 'UserMetaController@userMetaupdate')->name('user-generalinfo-update');
+Route::post('/dashboard/generalinfo/update', 'UserMetaController@userMetaupdate')->name('user-generalinfo-update')->middleware('verified');;
 
 // User Expertise Routes
-Route::get('dashboard/expertise', 'ExpertiseController@userExpertiseIndex')->name('dashboard.expertise')->middleware('auth');
-Route::post('dashboard/expertise', 'ExpertiseController@userExpertiseSave')->name('userexpertisesave')->middleware('auth');
-Route::get('dashboard/expertise/{id}', 'ExpertiseController@userExpertiseDelete')->name('userexpertisedelete')->middleware('auth');
+Route::get('dashboard/expertise', 'ExpertiseController@userExpertiseIndex')->name('dashboard.expertise')->middleware('verified');
+Route::post('dashboard/expertise', 'ExpertiseController@userExpertiseSave')->name('userexpertisesave')->middleware('verified');
+Route::get('dashboard/expertise/{id}', 'ExpertiseController@userExpertiseDelete')->name('userexpertisedelete')->middleware('verified');
 
 
 // User Achievements Routes
-Route::get('dashboard/achievements', 'AchievementController@userAchievementIndex')->name('dashboard.achievements')->middleware('auth');
-Route::post('dashboard/achievements', 'AchievementController@userAchievementSave')->name('userachievementsave')->middleware('auth');
-Route::get('dashboard/achievements/{id}', 'AchievementController@userAchievementDelete')->name('userachievementdelete')->middleware('auth');
+Route::get('dashboard/achievements', 'AchievementController@userAchievementIndex')->name('dashboard.achievements')->middleware('verified');
+Route::post('dashboard/achievements', 'AchievementController@userAchievementSave')->name('userachievementsave')->middleware('verified');
+Route::get('dashboard/achievements/{id}', 'AchievementController@userAchievementDelete')->name('userachievementdelete')->middleware('verified');
 
 // User Articlre Routes
-Route::get('dashboard/articles', 'ArticleController@userArticleIndex')->name('dashboard.articles')->middleware('auth');
-Route::post('dashboard/articles', 'ArticleController@userArticleSave')->name('userArticleSave')->middleware('auth');
-Route::get('dashboard/articles/{id}', 'ArticleController@userArticleDelete')->name('userArticleDelete')->middleware('auth');
+Route::get('dashboard/articles', 'ArticleController@userArticleIndex')->name('dashboard.articles')->middleware('verified');
+Route::post('dashboard/articles', 'ArticleController@userArticleSave')->name('userArticleSave')->middleware('verified');
+Route::get('dashboard/articles/{id}', 'ArticleController@userArticleDelete')->name('userArticleDelete')->middleware('verified');
 
 // User Award Routes
-Route::get('dashboard/awards', 'AwardController@userAwardIndex')->name('dashboard.awards')->middleware('auth');
-Route::post('dashboard/awards', 'AwardController@userAwardSave')->name('userAwardSave')->middleware('auth');
-Route::get('dashboard/awards/{id}', 'AwardController@userAwardDelete')->name('userAwardDelete')->middleware('auth');
+Route::get('dashboard/awards', 'AwardController@userAwardIndex')->name('dashboard.awards')->middleware('verified');
+Route::post('dashboard/awards', 'AwardController@userAwardSave')->name('userAwardSave')->middleware('verified');
+Route::get('dashboard/awards/{id}', 'AwardController@userAwardDelete')->name('userAwardDelete')->middleware('verified');
 
 // User Books Routes
-Route::get('dashboard/books', 'BookController@userBookIndex')->name('dashboard.books')->middleware('auth');
-Route::post('dashboard/books', 'BookController@userBookSave')->name('userBookSave')->middleware('auth');
-Route::get('dashboard/books/{id}', 'BookController@userBookDelete')->name('userBookDelete')->middleware('auth');
+Route::get('dashboard/books', 'BookController@userBookIndex')->name('dashboard.books')->middleware('verified');
+Route::post('dashboard/books', 'BookController@userBookSave')->name('userBookSave')->middleware('verified');
+Route::get('dashboard/books/{id}', 'BookController@userBookDelete')->name('userBookDelete')->middleware('verified');
 
 // User Current Position Routes
-Route::get('dashboard/current-positions', 'CurrentPositionController@userCurrentPositionIndex')->name('dashboard.current-positions')->middleware('auth');
-Route::post('dashboard/current-positions', 'CurrentPositionController@userCurrentPositionSave')->name('userCurrentPositionSave')->middleware('auth');
-Route::get('dashboard/current-positions/{id}', 'CurrentPositionController@userCurrentPositionDelete')->name('userCurrentPositionDelete')->middleware('auth');
+Route::get('dashboard/current-positions', 'CurrentPositionController@userCurrentPositionIndex')->name('dashboard.current-positions')->middleware('verified');
+Route::post('dashboard/current-positions', 'CurrentPositionController@userCurrentPositionSave')->name('userCurrentPositionSave')->middleware('verified');
+Route::get('dashboard/current-positions/{id}', 'CurrentPositionController@userCurrentPositionDelete')->name('userCurrentPositionDelete')->middleware('verified');
 
 // User Degree Routes
-Route::get('dashboard/degrees', 'DegreeController@userDegreeIndex')->name('dashboard.degress')->middleware('auth');
-Route::post('dashboard/degrees', 'DegreeController@userDegreeSave')->name('userDegreeSave')->middleware('auth');
-Route::get('dashboard/degrees/{id}', 'DegreeController@userDegreeDelete')->name('userDegreeDelete')->middleware('auth');
+Route::get('dashboard/degrees', 'DegreeController@userDegreeIndex')->name('dashboard.degress')->middleware('verified');
+Route::post('dashboard/degrees', 'DegreeController@userDegreeSave')->name('userDegreeSave')->middleware('verified');
+Route::get('dashboard/degrees/{id}', 'DegreeController@userDegreeDelete')->name('userDegreeDelete')->middleware('verified');
 
 // User Past Talks Routes
-Route::get('dashboard/past-talks', 'PastTalkController@userPastTaskIndex')->name('dashboard.past-talks')->middleware('auth');
-Route::post('dashboard/past-talks', 'PastTalkController@userPastTaskSave')->name('userPastTaskSave')->middleware('auth');
-Route::get('dashboard/past-talks/{id}', 'PastTalkController@userPastTaskDelete')->name('userPastTaskDelete')->middleware('auth');
+Route::get('dashboard/past-talks', 'PastTalkController@userPastTaskIndex')->name('dashboard.past-talks')->middleware('verified');
+Route::post('dashboard/past-talks', 'PastTalkController@userPastTaskSave')->name('userPastTaskSave')->middleware('verified');
+Route::get('dashboard/past-talks/{id}', 'PastTalkController@userPastTaskDelete')->name('userPastTaskDelete')->middleware('verified');
 
 
 // User Presentations Routes
-Route::get('dashboard/presentations', 'PresentationController@userPresentationIndex')->name('dashboard.presentations')->middleware('auth');
-Route::post('dashboard/presentations', 'PresentationController@userPresentationSave')->name('userPresentationSave')->middleware('auth');
-Route::get('dashboard/presentations/{id}', 'PresentationController@userPresentationDelete')->name('userPresentationDelete')->middleware('auth');
+Route::get('dashboard/presentations', 'PresentationController@userPresentationIndex')->name('dashboard.presentations')->middleware('verified');
+Route::post('dashboard/presentations', 'PresentationController@userPresentationSave')->name('userPresentationSave')->middleware('verified');
+Route::get('dashboard/presentations/{id}', 'PresentationController@userPresentationDelete')->name('userPresentationDelete')->middleware('verified');
 
 // User Workshops Routes
-Route::get('dashboard/workshops', 'WorkshopController@userWorkshopIndex')->name('dashboard.workshops')->middleware('auth');
-Route::post('dashboard/workshops', 'WorkshopController@userWorkshopSave')->name('userWorkshopSave')->middleware('auth');
-Route::get('dashboard/workshops/{id}', 'WorkshopController@userWorkshopDelete')->name('userWorkshopDelete')->middleware('auth');
+Route::get('dashboard/workshops', 'WorkshopController@userWorkshopIndex')->name('dashboard.workshops')->middleware('verified');
+Route::post('dashboard/workshops', 'WorkshopController@userWorkshopSave')->name('userWorkshopSave')->middleware('verified');
+Route::get('dashboard/workshops/{id}', 'WorkshopController@userWorkshopDelete')->name('userWorkshopDelete')->middleware('verified');
 
 // User Videos Routes
-Route::get('dashboard/videos', 'VideoController@userVideoIndex')->name('dashboard.videos')->middleware('auth');
-Route::post('dashboard/videos', 'VideoController@userVideoSave')->name('userVideoSave')->middleware('auth');
-Route::get('dashboard/videos/{id}', 'VideoController@userVideoDelete')->name('userVideoDelete')->middleware('auth');
+Route::get('dashboard/videos', 'VideoController@userVideoIndex')->name('dashboard.videos')->middleware('verified');
+Route::post('dashboard/videos', 'VideoController@userVideoSave')->name('userVideoSave')->middleware('verified');
+Route::get('dashboard/videos/{id}', 'VideoController@userVideoDelete')->name('userVideoDelete')->middleware('verified');
 
 
 // User Images Routes
-Route::get('dashboard/images', 'ImageController@userImageIndex')->name('dashboard.images')->middleware('auth');
-Route::post('dashboard/images', 'ImageController@userImageSave')->name('userImageSave')->middleware('auth');
-Route::get('dashboard/images/{id}', 'ImageController@userImageDelete')->name('userImageDelete')->middleware('auth');
+Route::get('dashboard/images', 'ImageController@userImageIndex')->name('dashboard.images')->middleware('verified');
+Route::post('dashboard/images', 'ImageController@userImageSave')->name('userImageSave')->middleware('verified');
+Route::get('dashboard/images/{id}', 'ImageController@userImageDelete')->name('userImageDelete')->middleware('verified');
 
 
 // User social-media-accounts Routes
-Route::get('dashboard/social-media-accounts', 'SocialMediaAccountController@userSocialMediaAccountIndex')->name('dashboard.social-media-accounts')->middleware('auth');
-Route::post('dashboard/social-media-accounts', 'SocialMediaAccountController@userSocialMediaAccountSave')->name('userSocialMediaAccountSave')->middleware('auth');
-Route::post('dashboard/social-media-accounts/update', 'SocialMediaAccountController@userSocialMediaAccountUpdate')->name('userSocialMediaAccountUpdate')->middleware('auth');
+Route::get('dashboard/social-media-accounts', 'SocialMediaAccountController@userSocialMediaAccountIndex')->name('dashboard.social-media-accounts')->middleware('verified');
+Route::post('dashboard/social-media-accounts', 'SocialMediaAccountController@userSocialMediaAccountSave')->name('userSocialMediaAccountSave')->middleware('verified');
+Route::post('dashboard/social-media-accounts/update', 'SocialMediaAccountController@userSocialMediaAccountUpdate')->name('userSocialMediaAccountUpdate')->middleware('verified');
 
 
 // User AVailbale For Routes
-Route::get('dashboard/available-for', 'AvailableForController@userAvailableForIndex')->name('dashboard.available-for')->middleware('auth');
-Route::post('dashboard/available-for', 'AvailableForController@userAvailableForSave')->name('userAvailableForSave')->middleware('auth');
-Route::post('dashboard/available-for/update', 'AvailableForController@userAvailableForUpdate')->name('userAvailableForUpdate')->middleware('auth');
+Route::get('dashboard/available-for', 'AvailableForController@userAvailableForIndex')->name('dashboard.available-for')->middleware('verified');
+Route::post('dashboard/available-for', 'AvailableForController@userAvailableForSave')->name('userAvailableForSave')->middleware('verified');
+Route::post('dashboard/available-for/update', 'AvailableForController@userAvailableForUpdate')->name('userAvailableForUpdate')->middleware('verified');
 
-
-
-// Route::get('dashboard/available-for', function(){
-//     return view('user.available-for');
-// })->name('dashboard.available-for')->middleware('auth');
 
 // Basic Details Update Reqest
-Route::post('/dashboard/update', 'HomeController@update')->name('user-basic-update');
+Route::post('/dashboard/update', 'HomeController@update')->name('user-basic-update')->middleware('verified');;
