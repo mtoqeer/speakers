0220cccf1276466e38cfd7a5de1d545b8beae51c
Muhammad Toqeer Iqbal <31422464+mtoqeer@users.noreply.github.com>
2019-03-19 13:09:34 +0500

    forms button updates

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -65,7 +65,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="biddaloy-btn-sm" style="cursor: pointer;border:none;">
                                     {{ __('Reset Password') }}
                                 </button>
                             </div>
