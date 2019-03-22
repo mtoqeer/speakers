cc241570330f6407945f31620f204bae3194a033
Muhammad Toqeer Iqbal <31422464+mtoqeer@users.noreply.github.com>
2019-03-23 01:03:09 +0500

    style updated

--- resources/views/user/images.blade.php
+++ resources/views/user/images.blade.php
@@ -5,30 +5,34 @@
 <div class="container">
     <div class="row">
         <div class="col-md-5">
-            
-            
-            <form method="POST" action="{{route('userImageSave')}}" enctype="multipart/form-data">
-                
-                @csrf
-                <input type="hidden" name="user_id" value="{{auth()->user()->id}}" class="form-control">
+
+                     
+            @if ($getImage->count() < 3)
+                <form method="POST" action="{{route('userImageSave')}}" enctype="multipart/form-data">
                     
-                <div class="form-group">
-                    <label for="images">Images</label>
-                    <input type="file" name="images" value="" class="form-control">
-                </div>
-                @if ($errors->has('images'))
-                    <span role="alert">
-                       <strong class="text-danger">{{ $errors->first('images') }}</strong>
-                    </span>
-                @endif
+                    @csrf
+                    <input type="hidden" name="user_id" value="{{auth()->user()->id}}" class="form-control">
+                        
+                    <div class="form-group">
+                        <label for="images">Images</label>
+                        <input type="file" name="images" value="" class="form-control">
+                    </div>
+                    @if ($errors->has('images'))
+                        <span role="alert">
+                        <strong class="text-danger">{{ $errors->first('images') }}</strong>
+                        </span>
+                    @endif
 
-                @if (session('message'))
-                <p id="image_added" class="alert alert-success">{{session('message')}}</p>
-                @endif
-                <button type="submit" class="btn btn-primary mb-5 mt-3 float-right" style="width:100%;">Add</button>
+                    @if (session('message'))
+                    <p id="image_added" class="alert alert-success">{{session('message')}}</p>
+                    @endif
+                    <button type="submit" class="btn btn-primary mb-5 mt-3 float-right" style="width:100%;">Add</button>
 
-            </form>
-
+                </form>
+            @else
+                <h4 class="alert alert-danger">You Have Reached Your Limit Of 3 Images</h4>
+            @endif
+           
         </div>
 
         <div class="col-md-5 offset-md-1">
