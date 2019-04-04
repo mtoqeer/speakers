6763d1a5a6d213bb81b3dc8fb05830d57cde1837
Muhammad Toqeer Iqbal <31422464+mtoqeer@users.noreply.github.com>
2019-04-04 10:26:38 +0500

    Message Change (Unapprove To Pending)

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -30,7 +30,7 @@
             </div>
 
             <div class="col-md-5 offset-md-1 border-left">
-                    <h4>Your Profile is <span style="color:green;">{{ucfirst(auth()->user()->status)}}</span> </h4>
+                    <h4>Your Profile s <span style="color:green;">{{ (auth()->user()->status == 'unapproved') ? "Pending" : "Approved" }}</span> </h4>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                         when an unknown printer took a galley of type and scrambled it to make a type 
