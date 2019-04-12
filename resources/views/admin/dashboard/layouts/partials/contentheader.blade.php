2514b83fccaeeb18fe9230a79118467d0883f8fb
Muhammad Toqeer Iqbal <31422464+mtoqeer@users.noreply.github.com>
2019-04-12 15:46:02 +0500

    Dashboard Link Fixed In Admin Panel

--- resources/views/admin/dashboard/layouts/partials/contentheader.blade.php
+++ resources/views/admin/dashboard/layouts/partials/contentheader.blade.php
@@ -6,7 +6,7 @@
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
-            <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+            <li class="breadcrumb-item"><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
             <li class="breadcrumb-item active"> @yield('pagename') </li>
           </ol>
         </div><!-- /.col -->
