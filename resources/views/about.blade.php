55eab58b7071ce3732545d2668a7377f2389077c
Muhammad Toqeer Iqbal <31422464+mtoqeer@users.noreply.github.com>
2019-04-15 21:01:49 +0500

    About Us Page Updated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.weblayout')
-@section('title', 'Home')
+@section('title', 'About Us')
 @section('customstyles')
     
 @endsection
@@ -9,7 +9,7 @@
     <!-- hero area start -->
     <section class="all-page-hero about-page">
             <div class="page-content container">
-                     <span class="about-active"> About</span>
+                     <span class="about-active"> About Us</span>
                  </div>
          </section>
      
@@ -20,17 +20,20 @@
                  <div class="row">
                      <div class="col-lg-6">
                          <div class="section-heading ">
+                                <h2>Our Mission</h2>
+                                <p>The mission of the Wisdom Speakers Bureau is to make finding the perfect speaker for your 
+                                    event simple. We will work with you directly to 
+                                    understand your specific event outcomes and budget in order to source the best speaker to 
+                                    ensure your attendees leave with the tools and resources they need.</p>
+
+                                    
                              <h2>About the Founder</h2>
                              <p>Gail Kingsbury has been in the event and public speaking industry for 30 years, working with companies 
                                  such as Kodak, Pizza Hut, Inc. Magazine and producing numerous events around the globe. She has worked with 
                                  the top speakers in the world, including Tony Robbins, Brendon Burchard, Brian Tracy, Dr. Barbara DeAngelis and Seth Godin, assisting with 
                                  everything from event production for 10 to 20,000 people, to focusing on making sure the right people are on the stage to best support the audience.</p>
      
-                             <h2>Our Mission</h2>
-                             <p>The mission of the Wisdom Speakers Bureau is to make finding the perfect speaker for your 
-                                 event simple. We will work with you directly to 
-                                 understand your specific event outcomes and budget in order to source the best speaker to 
-                                 ensure your attendees leave with the tools and resources they need.</p>
+                             
      
 
                          </div>
